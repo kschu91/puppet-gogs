4481c9ee0e89b50c6d713ddc0c81a2c15d316540
kevin.schu <kevin.schu@innovations4web.de>
2017-02-22 00:15:56 +0100

    adding more checks to gogs spec

--- spec/acceptance/gogs_spec.rb
+++ spec/acceptance/gogs_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper_acceptance'
 describe 'gogs' do
 
   context 'with defaults' do
-    it 'should listen on 3000' do
+    it 'should run' do
       pp = <<-EOS
         class { 'gogs': }
       EOS
@@ -13,31 +13,47 @@ describe 'gogs' do
       # apply_manifest(pp, :catch_changes => true)
     end
 
+    describe user('git') do
+      it { should exist }
+    end
+
+    describe group('git') do
+      it { should exist }
+    end
+
+    describe file('/opt/gogs') do
+      it { should be_directory }
+    end
+
+    describe file('/var/git') do
+      it { should be_directory }
+    end
+
     describe port(3000) do
       it { is_expected.to be_listening }
     end
   end
 
-  context 'with custom port' do
-    it 'should listen on 6000' do
-      pp = <<-EOS
-        class { '::gogs':
-            app_ini_sections => {
-                'server'   => {
-                    'HTTP_PORT' => 6000,
-                },
-            },
-        }
-      EOS
-
-      apply_manifest(pp, :catch_failures => true)
-      # @todo enable if gogs will restart only if version changed
-      # apply_manifest(pp, :catch_changes => true)
-    end
-
-    describe port(6000) do
-      it { is_expected.to be_listening }
-    end
-  end
+  # context 'with custom port' do
+  #   it 'should listen on 3210' do
+  #     pp = <<-EOS
+  #       class { '::gogs':
+  #           app_ini_sections => {
+  #               'server'   => {
+  #                   'HTTP_PORT' => 3210,
+  #               },
+  #           },
+  #       }
+  #     EOS
+  #
+  #     apply_manifest(pp, :catch_failures => true)
+  #     # @todo enable if gogs will restart only if version changed
+  #     # apply_manifest(pp, :catch_changes => true)
+  #   end
+  #
+  #   describe port(3210) do
+  #     it { is_expected.to be_listening }
+  #   end
+  # end
 
 end
