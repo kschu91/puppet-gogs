88ed414e4b173088aba78cbecbc69cf740da0dc0
kevin.schu <kevin.schu@innovations4web.de>
2017-02-22 17:55:14 +0100

    fix lint

--- manifests/service.pp
+++ manifests/service.pp
@@ -8,29 +8,29 @@ class gogs::service
 
   ) inherits gogs::params {
 
-  file { '/etc/init.d':
-    ensure => 'directory',
+  # file { '/etc/init.d':
+  #   ensure => 'directory',
+  #   owner  => 'root',
+  #   group  => 'root',
+  # }
+  #
+  #   ->
+
+  file { $gogs::params::init_script:
+    ensure => file,
+    source => $gogs::params::gogs_init_script,
     owner  => 'root',
     group  => 'root',
+    mode   => '0755',
   }
 
-    ->
-
-    file { $gogs::params::init_script:
-      ensure => file,
-      source => $gogs::params::gogs_init_script,
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0755',
-    }
-
-    ->
-
-    file { '/etc/init.d/functions':
-      ensure => created,
-      owner  => 'root',
-      group  => 'root',
-    }
+    # ->
+    #
+    # file { '/etc/init.d/functions':
+    #   ensure => created,
+    #   owner  => 'root',
+    #   group  => 'root',
+    # }
 
     ->
 
@@ -40,12 +40,12 @@ class gogs::service
       group  => 'root',
     }
 
-    create_resources('gogs::sysconfig', $sysconfig)
+  create_resources('gogs::sysconfig', $sysconfig)
 
-    service { $service_name:
-      ensure     => $service_ensure,
-      hasrestart => true, # let puppet start and stop (restart seems to be not working)
-      hasstatus  => true, # let puppet check for the process in process list (status command seems to be not working )
-      enable     => true,
-    }
+  service { $service_name:
+    ensure     => $service_ensure,
+    hasrestart => true, # let puppet start and stop (restart seems to be not working)
+    hasstatus  => true, # let puppet check for the process in process list (status command seems to be not working )
+    enable     => true,
+  }
 }
