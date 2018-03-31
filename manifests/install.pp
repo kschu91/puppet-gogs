e38bab69834ccb849ed1d30cfeab9e38a137c2eb
kevin.schu <kevin.schu@innovation4web.de>
2018-03-31 20:40:57 +0200

    Dependencies via "require" and do not delete shell scripts to ensure no change is done.

--- manifests/install.pp
+++ manifests/install.pp
@@ -19,67 +19,60 @@ class gogs::install (
     notify => Exec["permissions:${repository_root}"],
   }
 
-    -> file { $installation_directory:
-      ensure => 'directory',
-      owner  => $owner,
-      group  => $group,
-      notify => Exec["permissions:${installation_directory}"],
-    }
-
-    # @todo make log path configurable (app.ini: [log] ROOT_PATH && $::gogs::params::sysconfig[LOGPATH])
-    -> file { "${installation_directory}/log":
-      ensure => 'directory',
-      owner  => $owner,
-      group  => $group,
-    }
-
-    -> file { 'create:/tmp/download_gogs_from_github.sh':
-        ensure => 'file',
-        path   => '/tmp/download_gogs_from_github.sh',
-        source => 'puppet:///modules/gogs/download.sh',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0755',
-    }
-
-    -> file { 'create:/tmp/gogs_check_version.sh':
-        ensure => 'file',
-        path   => '/tmp/gogs_check_version.sh',
-        source => 'puppet:///modules/gogs/version.sh',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0755',
-    }
-
-    -> exec { 'download_gogs_from_github':
-      command     => '/tmp/download_gogs_from_github.sh',
-      user        => $owner,
-      group       => $group,
-      environment => [
-        "PUPPET_GOGS_INSTALLATION_DIRECTORY=${installation_directory}",
-        'PUPPET_GOGS_OS=linux',
-        "PUPPET_GOGS_ARCH=${::architecture}",
-        "PUPPET_GOGS_VERSION=${version}",
-      ],
-      logoutput   => true,
-      notify      => [
-        Exec['remove:/tmp/download_gogs_from_github.sh'],
-        Exec['remove:/tmp/gogs_check_version.sh'],
-        Service[$service_name],
-      ],
-      # only run if version has changed and needs to be updated
-      onlyif      => "/bin/bash /tmp/gogs_check_version.sh ${installation_directory} ${version}",
-      path        => ['/usr/bin', '/usr/sbin', '/bin'],
-    }
-
-  exec { 'remove:/tmp/download_gogs_from_github.sh':
-    command     => '/bin/rm -f /tmp/download_gogs_from_github.sh',
-    refreshonly => true,
+  file { $installation_directory:
+    ensure => 'directory',
+    owner  => $owner,
+    group  => $group,
+    notify => Exec["permissions:${installation_directory}"],
   }
 
-  exec { 'remove:/tmp/gogs_check_version.sh':
-    command     => '/bin/rm -f /tmp/gogs_check_version.sh',
-    refreshonly => true,
+  # @todo make log path configurable (app.ini: [log] ROOT_PATH && $::gogs::params::sysconfig[LOGPATH])
+  file { "${installation_directory}/log":
+    ensure => 'directory',
+    owner  => $owner,
+    group  => $group,
+  }
+
+  exec { 'download_gogs_from_github':
+    command     => '/tmp/download_gogs_from_github.sh',
+    user        => $owner,
+    group       => $group,
+    environment => [
+      "PUPPET_GOGS_INSTALLATION_DIRECTORY=${installation_directory}",
+      'PUPPET_GOGS_OS=linux',
+      "PUPPET_GOGS_ARCH=${::architecture}",
+      "PUPPET_GOGS_VERSION=${version}",
+    ],
+    logoutput   => true,
+    notify      => Service[$service_name],
+    require     => [
+      File[$repository_root],
+      File[$installation_directory],
+      File["${installation_directory}/log"],
+      File['/tmp/download_gogs_from_github.sh'],
+      File['/tmp/gogs_check_version.sh'],
+    ],
+    # only run if version has changed and needs to be updated
+    onlyif      => "/bin/bash /tmp/gogs_check_version.sh ${installation_directory} ${version}",
+    path        => ['/usr/bin', '/usr/sbin', '/bin'],
+  }
+
+  file { '/tmp/download_gogs_from_github.sh':
+    ensure => 'file',
+    path   => '/tmp/download_gogs_from_github.sh',
+    source => 'puppet:///modules/gogs/download.sh',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  file { '/tmp/gogs_check_version.sh':
+    ensure => 'file',
+    path   => '/tmp/gogs_check_version.sh',
+    source => 'puppet:///modules/gogs/version.sh',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
   }
 
   # just to make sure permissions are fine if owner or group is changed afterwards
