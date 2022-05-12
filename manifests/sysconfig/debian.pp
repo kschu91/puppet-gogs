3ca7c598f716a59a0d8349180c3397dbe1e16332
erdo <erdo@ichalsroot.de>
2022-05-12 23:54:07 +0200

    gogs::sysconfig::debian  - switched to systemd for debian

--- manifests/sysconfig/debian.pp
+++ manifests/sysconfig/debian.pp
@@ -3,34 +3,57 @@ define gogs::sysconfig::debian (
   $service_name           = $gogs::service_name,
   $installation_directory = $gogs::installation_directory,
   $owner                  = $gogs::owner,
+  $group                  = $gogs::group,
 
 ) {
 
-  # @see https://github.com/gogits/gogs/blob/master/scripts/init/centos/gogs
-  $sysconfig = {
-    'NAME'       => $service_name,
-    'USER'       => $owner,
-    'WORKINGDIR' => $installation_directory,
-    'DAEMON'     => "${installation_directory}/${service_name}",
-  }
-
-  file { "/etc/default/${service_name}":
-    ensure  => 'file',
-    content => template('gogs/sysconfig.erb'),
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
-    notify  => Service[$service_name],
-    before  => File["/etc/init.d/${service_name}"],
-  }
-
-  file { "/etc/init.d/${service_name}":
+  file { "/etc/systemd/system/${service_name}.service":
     ensure => file,
-    source => "${installation_directory}/scripts/init/debian/gogs",
+    source => "${installation_directory}/scripts/systemd/gogs.service",
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
+    replace => false,
+    notify => Service[$service_name],
+  }  ~>
+  exec { '/bin/systemctl daemon-reload':
+    refreshonly => true,
     notify => Service[$service_name],
   }
 
+  file_line { '${service_name}.service User':
+    ensure => present,
+    path   => "/etc/systemd/system/${service_name}.service",
+    line   => "User=${owner}",
+    match  => "^User=",
+    notify => Exec['/bin/systemctl daemon-reload'],
+  }
+  file_line { '${service_name}.service Group':
+    ensure => present,
+    path   => "/etc/systemd/system/${service_name}.service",
+    line   => "Group=${group}",
+    match  => "^Group=",
+    notify => Exec['/bin/systemctl daemon-reload'],
+  }
+  file_line { '${service_name}.service WorkingDirectory':
+    ensure => present,
+    path   => "/etc/systemd/system/${service_name}.service",
+    line   => "WorkingDirectory=${installation_directory}",
+    match  => "^WorkingDirectory=",
+    notify => Exec['/bin/systemctl daemon-reload'],
+  }
+  file_line { '${service_name}.service ExecStart':
+    ensure => present,
+    path   => "/etc/systemd/system/${service_name}.service",
+    line   => "ExecStart=${installation_directory}/gogs web",
+    match  => "^ExecStart=",
+    notify => Exec['/bin/systemctl daemon-reload'],
+  }
+  file_line { '${service_name}.service Environment':
+    ensure => present,
+    path   => "/etc/systemd/system/${service_name}.service",
+    line   => "Environment=USER=${owner} HOME=${installation_directory}",
+    match  => "^Environment=",
+    notify => Exec['/bin/systemctl daemon-reload'],
+  }
 }
